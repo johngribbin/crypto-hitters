e0f515ce088a0b611f5fb32fa6a45219f993fe00
John Gribbin <johnggribbin@gmail.com>
2020-04-18 13:12:22 -0400

    add log helper

--- src/helpers/getAllTickers.js
+++ src/helpers/getAllTickers.js
@@ -1,8 +1,9 @@
 import axios from "axios";
 
 export default async function getAllTickers() {
-  const proxyurl = "https://cors-anywhere.herokuapp.com/"; // When running locally Add this to front of "url" in fetch call like so - proxyurl + url
-  export const CMC_PRO_API_KEY = process.env.CMC_PRO_API_KEY || null; // replace null with your own CoinMarketCap API key when running locally
+  const proxyurl = "https://cors-anywhere.herokuapp.com/"; // When running locally Add this to front of the url in fetch call to avoid localhost CORS problems
+  const CMC_PRO_API_KEY = process.env.CMC_PRO_API_KEY || null; // replace null with your own CoinMarketCap API key when running locally
+  console.log(`cmc api key = `, CMC_PRO_API_KEY);
 
   const response = await fetch(
     "https://pro-api.coinmarketcap.com/v1/cryptocurrency/listings/latest",
