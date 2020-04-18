d4e39c834757d61facf61b505920d14458366282
John Gribbin <johnggribbin@gmail.com>
2020-04-18 14:39:27 -0400

    Remove test

--- src/App.js
+++ src/App.js
@@ -1,97 +1,97 @@
-import React, { Component } from 'react';
-import BurgerMenu from './components/BurgerMenu';
-import HeaderBar from './components/HeaderBar';
-import SearchBar from './components/SearchBar';
-import CryptoTable from './components/CryptoTable';
-import FooterBar from './components/FooterBar';
-import CryptoNews from './components/CryptoNews';
-import CryptoChart from './components/CryptoChart';
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
+import React, { Component } from "react";
+import BurgerMenu from "./components/BurgerMenu";
+import HeaderBar from "./components/HeaderBar";
+import SearchBar from "./components/SearchBar";
+import CryptoTable from "./components/CryptoTable";
+import FooterBar from "./components/FooterBar";
+import CryptoNews from "./components/CryptoNews";
+import CryptoChart from "./components/CryptoChart";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
-	// if user visits 'cryptoNews' or 'cryptoChart' pages the default chosen crypto asset is Bitcoin
-	state = {
-		searchTerm: '',
-		chosenCryptoName: 'Bitcoin',
-		chosenCryptoSymbol: 'BTC'
-	};
+  // if user visits 'cryptoNews' or 'cryptoChart' pages the default chosen crypto asset is Bitcoin
+  state = {
+    searchTerm: "",
+    chosenCryptoName: "Bitcoin",
+    chosenCryptoSymbol: "BTC",
+  };
 
-	// the term entered into 'searchBar' component
-	handleSearchTerm = searchTerm => {
-		this.setState({ searchTerm: searchTerm });
-	};
+  // the term entered into 'searchBar' component
+  handleSearchTerm = (searchTerm) => {
+    this.setState({ searchTerm: searchTerm });
+  };
 
-	// when 'news' arrow is clicked for a crypto asset, change 'chosenCryptoName' state
-	handleChosenCryptoName = chosenCryptoName => {
-		this.setState({ chosenCryptoName: chosenCryptoName });
-	};
+  // when 'news' arrow is clicked for a crypto asset, change 'chosenCryptoName' state
+  handleChosenCryptoName = (chosenCryptoName) => {
+    this.setState({ chosenCryptoName: chosenCryptoName });
+  };
 
-	// when 'news' arrow is clicked for a crypto asset, change 'chosenCryptoSymbol' state
-	handleChosenCryptoSymbol = chosenCryptoSymbol => {
-		this.setState({ chosenCryptoSymbol: chosenCryptoSymbol });
-	};
+  // when 'news' arrow is clicked for a crypto asset, change 'chosenCryptoSymbol' state
+  handleChosenCryptoSymbol = (chosenCryptoSymbol) => {
+    this.setState({ chosenCryptoSymbol: chosenCryptoSymbol });
+  };
 
-	render() {
-		return (
-			<BrowserRouter>
-				<Switch>
-					<Route
-						exact
-						path="/"
-						render={props => (
-							<div>
-								<BurgerMenu />
-								<HeaderBar />
-								<SearchBar
-									className={'hide-on-ipad'}
-									searchTerm={this.state.searchTerm}
-									handleSearchTerm={this.handleSearchTerm}
-								/>
+  render() {
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route
+            exact
+            path="/"
+            render={(props) => (
+              <div>
+                <BurgerMenu />
+                <HeaderBar />
+                <SearchBar
+                  className={"hide-on-ipad"}
+                  searchTerm={this.state.searchTerm}
+                  handleSearchTerm={this.handleSearchTerm}
+                />
 
-								<CryptoTable
-									searchTerm={this.state.searchTerm}
-									handleChosenCryptoName={this.handleChosenCryptoName}
-									handleChosenCryptoSymbol={this.handleChosenCryptoSymbol}
-								/>
-								<FooterBar />
-							</div>
-						)}
-					/>
+                <CryptoTable
+                  searchTerm={this.state.searchTerm}
+                  handleChosenCryptoName={this.handleChosenCryptoName}
+                  handleChosenCryptoSymbol={this.handleChosenCryptoSymbol}
+                />
+                <FooterBar />
+              </div>
+            )}
+          />
 
-					<Route
-						path="/news"
-						render={props => (
-							<div>
-								<CryptoNews
-									chosenCryptoName={this.state.chosenCryptoName}
-									chosenCryptoSymbol={this.state.chosenCryptoSymbol}
-								/>
-								<FooterBar />
-							</div>
-						)}
-					/>
+          <Route
+            path="/news"
+            render={(props) => (
+              <div>
+                <CryptoNews
+                  chosenCryptoName={this.state.chosenCryptoName}
+                  chosenCryptoSymbol={this.state.chosenCryptoSymbol}
+                />
+                <FooterBar />
+              </div>
+            )}
+          />
 
-					<Route
-						path="/chart"
-						render={props => (
-							<div>
-								<CryptoChart
-									chosenCryptoName={this.state.chosenCryptoName}
-									chosenCryptoSymbol={this.state.chosenCryptoSymbol}
-									chosenCryptoPrice={this.state.chosenCryptoPrice}
-								/>
-								<FooterBar />
-							</div>
-						)}
-					/>
+          <Route
+            path="/chart"
+            render={(props) => (
+              <div>
+                <CryptoChart
+                  chosenCryptoName={this.state.chosenCryptoName}
+                  chosenCryptoSymbol={this.state.chosenCryptoSymbol}
+                  chosenCryptoPrice={this.state.chosenCryptoPrice}
+                />
+                <FooterBar />
+              </div>
+            )}
+          />
 
-					<Route render={() => <h1>Page not found</h1>} />
-				</Switch>
-			</BrowserRouter>
-		);
-	}
+          <Route render={() => <h1>Page not found</h1>} />
+        </Switch>
+      </BrowserRouter>
+    );
+  }
 }
 
 export default App;
